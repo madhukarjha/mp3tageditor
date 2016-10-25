1409a5cd7cc80c5f87c8f8f3c9b387c36e6e664d
madhukar <madhukar@ubuntu.ubuntu-domain>
2016-10-25 23:33:09 +0530

    song name from parameter

--- start.js
+++ start.js
@@ -1,15 +1,25 @@
 var ffmetadata = require("ffmetadata");
 var metadata = require('./metadata');
- 
-// Read song.mp3 metadata 
-ffmetadata.read("song.mp3", function(err, data) {
-    if (err) console.error("Error reading metadata", err);
-    else console.log(data);
+
+var songname = "";
+process.argv.forEach(function (val, index, array) {
+    console.log(songname);
+    songname = val;
 });
- 
-// Set the artist for song.mp3 
-var data = metadata;
-ffmetadata.write("song.mp3", data, function(err) {
-    if (err) console.error("Error writing metadata", err);
-    else console.log("Data written");
-});
+
+
+if (songname) {
+    // Read song.mp3 metadata 
+    ffmetadata.read(songname, function (err, data) {
+        if (err) console.error("Error reading metadata", err);
+        else console.log(data);
+    });
+}
+
+
+// // Set the artist for song.mp3 
+// var data = metadata;
+// ffmetadata.write("song.mp3", data, function (err) {
+//     if (err) console.error("Error writing metadata", err);
+//     else console.log("Data written");
+// });
